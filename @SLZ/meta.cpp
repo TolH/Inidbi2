afc0753c6215bbff8baf42600638f6a5eb06ccc6
TohL <TolH@users.noreply.github.com>
2020-02-03 22:51:15 -0500

    Add files via upload

---
+++ @SLZ/meta.cpp
@@ -0,0 +1,4 @@
+protocol = 1;
+publishedid = 647452409;
+name = "SLZ";
+timestamp = 5248200718092358751;
---
+++ @SLZ/mod.cpp
@@ -0,0 +1,6 @@
+name = "seelenlos Zeus 1.7";
+picture = "slz.paa";
+tooltip = "seelenlos Zeus";
+overview = "This mod creates a zeus and assign the player if he is whitelisted!";
+author = "seelenlos";
+logoOver = "slz.paa";
